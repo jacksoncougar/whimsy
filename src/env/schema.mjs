f37580f9c25b22ca100af96a3f7a8e54f465acdd
Jackson C Wiebe <jacksoncougar@gmail.com>
2023-01-02 19:07:52 -0700

    fix: remove schema validation

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,8 +6,8 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
-  NODE_ENV: z.enum(["development", "test", "production"]),
+  // DATABASE_URL: z.string().url(),
+  NODE_ENV: z.enum([ "development", "test", "production" ]),
 });
 
 /**
